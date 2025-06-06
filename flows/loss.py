b81d65ccc04789f864617b7eb5a87d3f03a720c1
Ali Ozan Memetoğlu <69462206+aliozanmemeto@users.noreply.github.com>
2025-06-06 20:34:24 +0200

    Delete flows directory

--- flows/loss.py
+++
@@ -1,9 +0,0 @@
-def likelihood(X_train, model, device):
-    ##########################################################
-    # YOUR CODE HERE
-    X_train = X_train.to(device)
-    batch_log_prob = model.log_prob(X_train)
-    loss = -batch_log_prob.mean(dim=0)
-    ##########################################################
-
-    return loss
